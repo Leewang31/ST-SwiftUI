c97520dc25c90a9ea1d2f42c27f06216c8b6f04f
갸니 <kiwan@gyaniui-MacBookPro.local>
2025-02-14 18:03:29 +0900

    Section 6. Set up navigation between list and detail

---
+++ Landmarks/Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by 갸니 on 2/14/25.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}

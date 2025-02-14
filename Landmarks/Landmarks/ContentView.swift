f3307554e71180386c07c5e519992fa31a15e5ed
갸니 <kiwan@gyaniui-MacBookPro.local>
2025-02-14 15:54:35 +0900

    Section3. Combine views using stacks

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -2,9 +2,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer() // 가변공간 생성 다른 뷰들 사이에 유동적으로 공간을 추가할 수 있습니다
+                Text("California")
+                    .font(.subheadline)
+            }
         }
+        .padding() //padding()은 SwiftUI에서 뷰 주위에 여백을 추가하는 데 사용됩니다.
     }
 }
 

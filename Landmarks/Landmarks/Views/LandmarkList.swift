c22817c873bd96243d8b1a6d0b5b7f6127e24e4f
갸니 <kiwan@gyaniui-MacBookPro.local>
2025-02-16 00:35:23 +0900

    Section 3. Add a control to toggle the state

--- Landmarks/Landmarks/Views/LandmarkList.swift
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -6,8 +6,7 @@ struct LandmarkList: View {
      * @State는 뷰 내부에서 값이 변경될 수 있는 상태를 저장하는 속성입니다.
      * 이 값이 변경될 때 SwiftUI는 해당 뷰를 다시 렌더링하여 최신 상태를 반영합니다.
      */
-//    @State private var showFavoritesOnly = false
-    @State private var showFavoritesOnly = true
+    @State private var showFavoritesOnly = false
 
     var filteredLandmarks: [Landmark] {
         landmarks.filter { landmark in
@@ -18,16 +17,23 @@ struct LandmarkList: View {
     var body: some View {
         NavigationSplitView{
             // 필터링된 값을 보여준다 showFavoritesOnly에 따라
-            List(filteredLandmarks) { landmark in
-                // 위치 이동 값 전달 landmark
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
+            List{
+                Toggle(isOn: $showFavoritesOnly) {
+                                    Text("Favorites only")
                 }
-                // 화면 노출 컴포넌트
-                label: {
-                    LandmarkRow(landmark: landmark)
+
+                ForEach(filteredLandmarks) { landmark in
+                    // 위치 이동 값 전달 landmark
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    }
+                    // 화면 노출 컴포넌트
+                    label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
+            .animation(.default, value: filteredLandmarks)
             // 상단 노출 타이틀
             .navigationTitle("Landmarks")
         } detail: {

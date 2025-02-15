23d94203f13c7ca2c80910c34169011da40d05a2
갸니 <kiwan@gyaniui-MacBookPro.local>
2025-02-16 02:56:16 +0900

    Section 1. Create drawing data for a badge view

---
+++ DrawingAnimation/DrawingAnimation/DrawingAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingAnimationApp.swift
+//  DrawingAnimation
+//
+//  Created by 갸니 on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct DrawingAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

cdc39ab9a9b76052387fe2310eecaef51f884541
갸니 <kiwan@gyaniui-MacBookPro.local>
2025-02-16 00:26:16 +0900

    Section 2. Filter the list view

--- Landmarks/Landmarks/Views/LandmarkList.swift
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -2,9 +2,23 @@ import SwiftUI
 
 
 struct LandmarkList: View {
+    /**
+     * @State는 뷰 내부에서 값이 변경될 수 있는 상태를 저장하는 속성입니다.
+     * 이 값이 변경될 때 SwiftUI는 해당 뷰를 다시 렌더링하여 최신 상태를 반영합니다.
+     */
+//    @State private var showFavoritesOnly = false
+    @State private var showFavoritesOnly = true
+
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+
     var body: some View {
         NavigationSplitView{
-            List(landmarks) { landmark in
+            // 필터링된 값을 보여준다 showFavoritesOnly에 따라
+            List(filteredLandmarks) { landmark in
                 // 위치 이동 값 전달 landmark
                 NavigationLink {
                     LandmarkDetail(landmark: landmark)
